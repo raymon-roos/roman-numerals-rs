0c1bfebc7d56f0d04181f709dc35ab4439210a20
Raymon Roos <raymon.roos@hotmail.com>
2024-06-22 00:08:55 +0200

    feat: first version
    Takes an integer as argument on the CLI, and returns a string containing
    the Roman numeral corresponding to the number.

---
+++ src/main.rs
@@ -0,0 +1,75 @@
+use std::{env, usize};
+
+const NUMERALS: [(&str, usize); 13] = [
+    ("M", 1000),
+    ("CM", 900),
+    ("D", 500),
+    ("CD", 400),
+    ("C", 100),
+    ("XC", 90),
+    ("L", 50),
+    ("XL", 40),
+    ("X", 10),
+    ("IX", 9),
+    ("V", 5),
+    ("IV", 4),
+    ("I", 1),
+];
+
+fn main() {
+    let i = env::args().nth(1).map_or(100, |arg| {
+        arg.parse::<usize>()
+            .expect("Argument should be an integer!")
+    });
+
+    println!("{}", to_roman(i));
+}
+
+fn to_roman(p: usize) -> String {
+    let numeral: (String, usize) = NUMERALS
+        .iter()
+        .skip_while(|&(_symbol, value)| *value > p)
+        .map(|&(symbol, value)| (symbol.to_string(), value))
+        .fold(
+            (String::from(""), 0),
+            |(mut symbols, mut total), (symbol, val)| {
+                let remainder = p - total;
+                let multiplier = (remainder - (remainder % val)) / val;
+                total += multiplier * val;
+                symbols.push_str(symbol.repeat(multiplier).as_ref());
+                (symbols, total)
+            },
+        );
+
+    numeral.0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn three() {
+        assert_eq!("III", to_roman(3));
+    }
+
+    #[test]
+    fn two_hundred_seven() {
+        assert_eq!("CCVII", to_roman(207));
+    }
+
+    #[test]
+    fn seven_hundred_eighty_nine() {
+        assert_eq!("DCCLXXXIX", to_roman(789));
+    }
+
+    #[test]
+    fn eight_hundred_eight() {
+        assert_eq!("DCCCLXXXVIII", to_roman(888));
+    }
+
+    #[test]
+    fn nineteen_hundred_eighteen() {
+        assert_eq!("MCMXVIII", to_roman(1918));
+    }
+}
