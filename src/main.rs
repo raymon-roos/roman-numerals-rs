2acc24235b79e8ce53437f6aae0180b96c5753ca
Raymon Roos <raymon.roos@hotmail.com>
2024-10-03 23:23:32 +0200

    feat: add `from_numeral` function to turn Roman numerals into an int
    First time using the `matches!()` macro, pretty neat!
    But man, this is probably not the most elegant solution, though it makes
    excellent use of Rust's standard library and pattern matching.
    
    It takes a string, and turns it into an iterator of chunks of
    characters. But the chunk size is predicated on a callback. This
    callback checks if this chuck is a Roman digit that is made of multiple
    parts - i.e. IV. So CDXCIV turns into an iterator over:
    `[ &['C', 'D'], &['X', 'C'], &['I', 'V'] ]`, whereas VIII becomes:
    `[&['V'], &['I'], &['I'], &['I']]`. It then pattern-matches the contents of
    these chunks against the list of known Roman numeral digits to get the
    integer value.
    
    I also split up my existing test cases into `from_numeral` and
    `to_numeral` versions. I added another case for 494, to check the
    chunking logic mentioned above in particular.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, usize};
+use core::panic;
+use std::env;
 
 const NUMERALS: [(&str, usize); 13] = [
     ("M", 1000),
@@ -23,6 +24,8 @@ fn main() {
     });
 
     println!("{}", to_roman(i));
+
+    from_roman("MCMXVIII");
 }
 
 fn to_roman(p: usize) -> String {
@@ -45,32 +48,94 @@ fn to_roman(p: usize) -> String {
     numeral.0
 }
 
+fn from_roman(input_str: &str) -> usize {
+    let input_symbols = input_str.chars().collect::<Vec<char>>();
+    let input_roman_digits = input_symbols.chunk_by(|a, b| {
+        matches!(
+            (a, b),
+            ('C', 'M') | ('C', 'D') | ('X', 'C') | ('X', 'L') | ('I', 'X') | ('I', 'V')
+        )
+    });
+
+    let mut num = 0;
+    for input_roman_digit in input_roman_digits {
+        dbg!(&input_roman_digit);
+        if let Some((_symbol, value)) = NUMERALS.iter().find(|&(symbol, _val)| {
+            match (input_roman_digit.first(), input_roman_digit.get(1)) {
+                (Some(a), Some(b)) => *symbol == format!("{a}{b}"),
+                (None, Some(b)) => *symbol == b.to_string(),
+                (Some(a), None) => *symbol == a.to_string(),
+                _ => panic!("Invalid numeral!"),
+            }
+        }) {
+            num += value;
+        }
+    }
+
+    num
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn three() {
+    fn three_from_roman() {
         assert_eq!("III", to_roman(3));
     }
 
     #[test]
-    fn two_hundred_seven() {
+    fn three_to_roman() {
+        assert_eq!(from_roman("III"), 3);
+    }
+
+    #[test]
+    fn two_hundred_seven_from_roman() {
         assert_eq!("CCVII", to_roman(207));
     }
 
     #[test]
-    fn seven_hundred_eighty_nine() {
+    fn two_hundred_seven_to_roman() {
+        assert_eq!(from_roman("CCVII"), 207);
+    }
+
+    #[test]
+    fn four_hundred_ninety_four_from_roman() {
+        assert_eq!("CDXCIV", to_roman(494));
+    }
+
+    #[test]
+    fn four_hundred_ninety_four_to_roman() {
+        assert_eq!(from_roman("CDXCIV"), 494);
+    }
+
+    #[test]
+    fn seven_hundred_eighty_nine_from_roman() {
         assert_eq!("DCCLXXXIX", to_roman(789));
     }
 
     #[test]
-    fn eight_hundred_eight() {
+    fn seven_hundred_eighty_nine_to_roman() {
+        assert_eq!(from_roman("DCCLXXXIX"), 789);
+    }
+
+    #[test]
+    fn eight_hundred_eight_from_roman() {
         assert_eq!("DCCCLXXXVIII", to_roman(888));
     }
 
     #[test]
-    fn nineteen_hundred_eighteen() {
-        assert_eq!("MCMXVIII", to_roman(1918));
+    fn eight_hundred_eight_to_roman() {
+        assert_eq!(from_roman("DCCCLXXXVIII"), 888);
+    }
+
+    #[test]
+    fn nineteen_hundred_eighteen_to_roman() {
+        assert_eq!(from_roman("MCMXVIII"), 1918);
+    }
+
+    #[test]
+    fn nineteen_hundred_eighteen_from_roman() {
+        assert_eq!(from_roman("MCMXVIII"), 1918);
     }
 }
